a2ad0d436ea1c89e9eb1bb244e075912f2b91f3b
HP <akshaykaitheri@gmail.com>
2023-07-06 12:50:50 +0530

    commit

--- src/pagepack/page1.java
+++ src/pagepack/page1.java
@@ -1,5 +1,54 @@
 package pagepack;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class page1 {
+	WebDriver driver;
+	By myaccount=By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a");
+	By regstrn=By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a");
+	By firstname=By.xpath("//*[@id=\"input-firstname\"]");
+	By lastname=By.xpath("//*[@id=\"input-lastname\"]");
+	By email=By.xpath("//*[@id=\"input-email\"]");
+	By telephone=By.xpath("//*[@id=\"input-telephone\"]");
+	By password=By.xpath("//*[@id=\"input-password\"]");
+	By psswrdcnfrm=By.xpath("//*[@id=\"input-confirm\"]");
+	By checkbox=By.xpath("//*[@id=\"content\"]/form/div/div/input[1]");
+	By continue1=By.xpath("//*[@id=\"content\"]/form/div/div/input[2]");
+	By continue2=By.xpath("//*[@id=\"content\"]/div/div/a");
+	
+	public page1(WebDriver driver) {
+		this.driver=driver;
+		
+	}
+
+public void click()
+{
+	driver.findElement(myaccount).click();
+}
+public void click2()
+{
+	driver.findElement(regstrn).click();
+}
+public void setvalues(String fname,String lname,String emailid,String tele,String pass,String passconfrm)
+{
+	driver.findElement(firstname).sendKeys(fname);
+	driver.findElement(lastname).sendKeys(lname);
+	driver.findElement(email).sendKeys(emailid);
+	driver.findElement(telephone).sendKeys(tele);
+	driver.findElement(password).sendKeys(pass);
+	driver.findElement(psswrdcnfrm).sendKeys(passconfrm);
+}
+public void click3() {
+	driver.findElement(checkbox).click();
+}
+public void click4()
+{
+	driver.findElement(continue1).click();
+}
+public void click5()
+{
+	driver.findElement(continue2).click();
+}
 
 }
--- src/testpack/test1.java
+++ src/testpack/test1.java
@@ -1,5 +1,36 @@
 package testpack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pagepack.page1;
+
 public class test1 {
+	WebDriver driver;
+	@BeforeTest
+	public void setup()
+	{
+		driver= new ChromeDriver();
+		driver.get("https://naveenautomationlabs.com/opencart");
+		driver.manage().window().maximize();
+	}
+	@Test
+	public void test()throws InterruptedException
+	{
+		page1 ob= new page1(driver);
+		ob.click();
+		Thread.sleep(2000);
+		ob.click2();
+		Thread.sleep(3000);
+		ob.setvalues("Akshay","ak","akshay765@gmail.com","9961844869","akzy123","akzy123");
+		Thread.sleep(4000);
+		ob.click3();
+		Thread.sleep(2000);
+		ob.click4();
+		Thread.sleep(3000);
+		ob.click5();
+	}
 
 }
